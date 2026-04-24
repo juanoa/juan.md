00b5bd7fb599eecb68a8b0f2b83efb224b4fd7c8
Juan Otálora <juan@mito.ai>
2026-04-24 22:54:14 +0200

    style: dark-mode

--- apps/onedotzeroone/app/layout.tsx
+++ apps/onedotzeroone/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { ThemeProvider } from "@/components/theme-provider";
 import "@juan/ui/styles.css";
 
 export const metadata: Metadata = {
@@ -14,11 +15,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
-      <body className="flex min-h-screen flex-col">
-        <Header />
-        <main className="flex-1">{children}</main>
-        <Footer />
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="bg-background text-foreground flex min-h-screen flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
