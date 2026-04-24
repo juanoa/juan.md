69b8d0527a7c66aa16a58938b882ac9b53e88249
Juan Otálora <juan@mito.ai>
2026-04-25 00:29:33 +0200

    revert(101): revert today commits

--- apps/onedotzeroone/app/thesis/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <article className="max-w-xl mx-auto mt-20">{children}</article>
-  );
-}
