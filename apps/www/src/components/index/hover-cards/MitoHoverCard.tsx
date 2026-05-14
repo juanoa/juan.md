1347586445c1e3a5f022ab5c8f45d42a23092280
Juan Otálora <juan@mito.ai>
2026-05-14 23:52:10 +0200

    feat: add mito video

--- apps/www/src/components/index/hover-cards/MitoHoverCard.tsx
+++ apps/www/src/components/index/hover-cards/MitoHoverCard.tsx
@@ -7,10 +7,7 @@ interface Props {
 export const MitoHoverCard = ({ text }: Props) => {
   return (
     <HomePageHoverCard text={text}>
-      <div className="flex gap-2">
-        <img src="/mito-logo.webp" className="size-10" />
-        <div className="pt-1 leading-4">Video creation tools powered by AI</div>
-      </div>
+      <video src="/mito-video.webm" autoPlay loop muted playsInline />
     </HomePageHoverCard>
   );
 };
