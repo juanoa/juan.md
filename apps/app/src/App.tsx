02c5e5a1185de7fe38eca2b802eab3e394293320
Juan Otálora <juan@mito.ai>
2026-06-10 13:11:16 +0200

    chore: first commit

---
+++ apps/app/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-background text-foreground flex min-h-screen items-center justify-center font-mono">
+      <h1 className="text-sm font-medium">Hello World!</h1>
+    </div>
+  );
+}
